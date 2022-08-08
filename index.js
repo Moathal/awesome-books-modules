c2f8b7b506403d514e67de4e3ed792142603c69e
Moathal <moathal.s.kachi@gmail.com>
2022-08-08 21:42:06 +0300

    Add Methods and books array class module
    Add book class object as module
    
    Add eventListener to add book navigate and remove book

---
+++ index.js
@@ -0,0 +1,39 @@
+import DateTime from './node_modules/luxon/src/datetime.js';
+import Book from './modules/book.js';
+import BookM from './modules/methods.js';
+
+const booksList = document.querySelector('.books-list');
+const methods = new BookM();
+const addBook = document.getElementById('add-book');
+const newBook = document.getElementById('book');
+const newAuthor = document.getElementById('author');
+const pages = document.querySelectorAll('.page');
+const header = document.querySelector('header');
+const date = document.querySelector('.date');
+let link = 'a';
+
+date.innerText = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+if (localStorage.getItem('books') !== null) methods.Books = JSON.parse(localStorage.getItem('books'));
+
+methods.resetBooksList(booksList);
+
+booksList.addEventListener('click', (e) => {
+  if (e.target.className === 'rem-btn') {
+    methods.deleteBook(e.target.id, booksList);
+  }
+});
+
+addBook.addEventListener('click', () => {
+  const book = new Book(newBook.value, newAuthor.value);
+  methods.Books.push(book);
+  window.localStorage.setItem('books', JSON.stringify(methods.Books));
+  methods.resetBooksList(booksList);
+});
+
+header.addEventListener('click', (e) => {
+  link = e.target.innerText.replace(/\s/g, '');
+  if (link === 'List' || link === 'Addnew' || link === 'Contact') {
+    pages.forEach((page) => page.classList.remove('active'));
+    document.getElementById(link).classList.add('active');
+  }
+});
---
+++ modules/book.js
@@ -0,0 +1,6 @@
+export default class Book {
+  constructor(book, author) {
+    this.book = book;
+    this.author = author;
+  }
+}
